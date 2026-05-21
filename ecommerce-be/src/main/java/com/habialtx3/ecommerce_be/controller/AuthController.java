48bee1b43e9f681d7a7aef59a7bd0f01326585ac
Muhammad Reza Pahlevi <rezaworku31@gmail.com>
2026-05-21 09:54:55 +0700

    chore : intialize folders

---
+++ ecommerce-be/src/main/java/com/habialtx3/ecommerce_be/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.habialtx3.ecommerce_be.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @GetMapping(path = "/api/halo")
+    public String create() {
+        return "Hello world";
+    }
+}
