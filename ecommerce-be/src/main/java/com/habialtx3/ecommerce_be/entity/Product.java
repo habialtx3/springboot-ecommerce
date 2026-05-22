19e4b006280eca61d61575aa5e1804ebac5a50de
Muhammad Reza Pahlevi <rezaworku31@gmail.com>
2026-05-22 14:50:30 +0700

    chore : int category and product entity

--- ecommerce-be/src/main/java/com/habialtx3/ecommerce_be/entity/Product.java
+++ ecommerce-be/src/main/java/com/habialtx3/ecommerce_be/entity/Product.java
@@ -1,7 +1,6 @@
 package com.habialtx3.ecommerce_be.entity;
 
 import jakarta.persistence.*;
-import jdk.jfr.Category;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -21,15 +20,19 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "category_id", nullable = false)
-//    private Category category;
 
     @Column(nullable = false)
     private String name;
 
     @Column(unique = true, nullable = false)
     private String slug;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "category_id",
+    nullable = false,
+    foreignKey = @ForeignKey(name = "FK_PRODUCT_CATEGORY"))
+    private Category category;
 
     @Column(columnDefinition = "TEXT")
     private String description;
@@ -45,4 +48,6 @@ public class Product {
 
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
+
+
 }
